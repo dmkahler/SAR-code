a2a1737d2c6bdac9ad75b1dc6d7ff0c7aee460b4
David Kahler <david.m.kahler@gmail.com>
2024-04-23 21:54:47 -0400

    create virtual layer

---
+++ mergeTiffs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Merge multiple Geotiffs for further analysis - also included, NDWI calculation
+
+import os
+import glob
+from osgeo import gdal
+
+ls = glob.glob('/Volumes/dmk/Pittsburgh_Sept2023_psscene_analytic_sr_udm2/Analytic/*.tif') # where [...] is the remainder of the path of all geotiff files
+# Make sure all files are in the same projection
+
+# pixel size:
+# QGIS: check pixel size under properties -> information -> pixel size
+# Note: keep any negative signs
+dx = 3
+dy = -3
+
+vrt = gdal.BuildVRT("mergeTiffs.vrt, ls")
+gdal.Translate("mergeTiffs.tif", vrt, xRes = dx, yRes = dy)
+vrt = None
